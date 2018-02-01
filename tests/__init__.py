62a941dccec7164b3c09a4e0a0c09047e3afbccf
C?dric Krier <ced@b2ck.com>
2018-02-01 17:13:55 +0100

    Update to 4.7

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
 # This file is part sale_jreport module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from .test_sale_jreport import suite
+try:
+    from trytond.modules.sale_jreport.tests.test_sale_jreport import suite
+except ImportError:
+    from .test_sale_jreport import suite
 
 __all__ = ['suite']
